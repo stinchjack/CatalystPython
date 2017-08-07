da08d8aaaf81a9fbd8755ab33fccae274a7b6d4e
Jack Stinchcombe <stinchjack@gmail.com>
2017-08-07 12:20:05 +1200

    README tweak

--- user_upload.py
+++ user_upload.py
@@ -1,7 +1,6 @@
 """ Author: Jack Stinchcombe
 email: stinchjack@gmail.com
 Updated: 6 August 2017
-github:
 """
 import os
 import argparse
@@ -15,17 +14,15 @@ def  help():
     Help output def
   """
 
-  helpText = """--file [csv file name] - this is the name of the CSV to be parsed
-  --create_table - this will cause the MySQL users table to be built (and no further
-  action will be taken)
-  --dry_run - this will be used with the --file directive in the instance that we want to run the
-  script but not insert into the DB. All other defs will be executed, but the database won't
-  be altered.
-  -u - MySQL username
-  -p - MySQL password
-  -h - MySQL host
-  --dbname - specify a DB name
-  --help - output this help """
+  helpText = """
+     --file [csv file name] - this is the name of the CSV to be parsed (default users.csv if not specified)
+     --create_table - this will cause the MySQL users table to be built (and no further action will be taken)
+     --dry_run - this will be used with the --file directive in the instance that we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered.
+     -u - MySQL username
+     -p - MySQL password
+     -h - MySQL host (default localhost if not specified)
+     --dbname - specify a DB name (default catalystUsers if not specified)
+     --help - output help """
 
   print helpText
 
